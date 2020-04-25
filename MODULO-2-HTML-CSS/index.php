9a0ac7b1777e74c44ddb57a2dfb6cfacc43272b4
MiqueiasSevero <miq.severo@gmail.com>
2020-04-25 07:37:02 -0300

    arquivos do curso php udemy

---
+++ MODULO-2-HTML-CSS/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" href="access/css/estilo.css">
+    <title>Curso PHP</title>
+</head>
+<body>
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Índice dos Exercícios</h2>
+    </header>
+    <main class="principal">
+        <div class="conteudo">
+            <nav class="modulos">
+                <div class="modulo verde">
+                    <h3>Módulo 1</h3>
+                    <ul>
+                        <li><a href="exercicio.php?dir=teste&file=teste">Exercício A</a></li>
+                       
+                    </ul>
+                </div>
+
+        </div>
+
+    </main>
+    <footer class="rodape">
+        COD3R & ALUNOS <?php echo date('Y'); ?>
+    </footer>
+</body>
+</html>
