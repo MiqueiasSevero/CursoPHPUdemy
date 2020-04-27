cc6dcee5a931cdadd9fe3d39b543029260c50543
MiqueiasSevero <miq.severo@gmail.com>
2020-04-27 07:54:06 -0300

    intro PHP

---
+++ MODULO-3-INTRO-PHP/basico/ola.php
@@ -0,0 +1,15 @@
+<div class="titulo">Olá PHP</div>
+<h2>Exemplo A</h2>
+<?php
+    echo 'Olá ';
+    echo "<br>";
+    echo 'Mundo!';
+ ?>
+
+
+<h2>Exemplo B</h2>
+<!--Outra forma de imprimir resultados no php-->
+<?= "<br>Outra forma de expressar" ?>
+
+<?php
+phpinfo();
